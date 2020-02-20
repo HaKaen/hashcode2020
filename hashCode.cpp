34bf62e3d6b429b60da47098b94953ab7a900d4b
Benoit Robillard <berobillard@airfrance.fr>
2020-02-20 18:17:02 +0000

    read/write

--- hashCode.cpp
+++ hashCode.cpp
@@ -8,120 +8,176 @@
 #include<string>
 
 using namespace std;
+hash<string> hash_fn;
 
 //Indexed object to easy sort and outputs
-class Pizza{
+class Book{
 public:
-	Pizza(int,int);
+	Book(int,int);
 	int index_m;
-	int nbSlices_m;
+	int score_m;
 	bool isAssigned_m;
 };
 
-Pizza::Pizza(int index_p, int nbSlices_p):
+class Library{
+public:
+	Library(int,int,int,int);
+	int index_m;
+	int nbBooks_m;
+	int nbDaysToFinish_m;
+	int nbBooksShippedByDay_m;
+	vector<Book*> vBooksInLibrary_m;
+};
+
+Book::Book(int index_p, int score_p):
 index_m(index_p),
-nbSlices_m(nbSlices_p),
+score_m(score_p),
 isAssigned_m(false)
 {
 }
 
+Library::Library(int index_p, int nbBooks_p, int nbDaysToFinish_p, int nbBooksShippedByDay_p):
+index_m(index_p),
+nbBooks_m(nbBooks_p),
+nbDaysToFinish_m(nbDaysToFinish_p),
+nbBooksShippedByDay_m(nbBooksShippedByDay_p),
+vBooksInLibrary_m()
+{
+}
+
 //Object definition
-class PizzaProblem{
+class BookProblem{
 public:
 	//Constructor
-	PizzaProblem();
-	~PizzaProblem();
+	BookProblem();
+	~BookProblem();
 	//Different types, int or bool as examples
-	int maxSlices_m;
-	int nbPizzas_m;
-	//Pizzas in the problem
-	vector<Pizza*> pizzas_m;
+	int nbBooks_m;
+	int nbLibraries_m;
+	int nbDays_m;
+	//Books in the problem
+	vector<Book*> vBooks_m;
 	//Solution
-	vector<Pizza*> pizzasToOrder_m;
+	vector<Library*> vLibraries_m;
 	//Optimize
 	void optimize();
 	//Eval
 	int eval();
 	//Lecture d'un objet
-	friend istream& operator>> (istream&,PizzaProblem&);
+	friend istream& operator>> (istream&,BookProblem&);
 	//Ecriture d'un objet
-	friend ostream& operator<< (ostream&,const PizzaProblem&);
+	friend ostream& operator<< (ostream&,const BookProblem&);
 };
 
-PizzaProblem::PizzaProblem():
-maxSlices_m(),
-nbPizzas_m(),
-pizzas_m(),
-pizzasToOrder_m()
+BookProblem::BookProblem():
+nbBooks_m(),
+nbLibraries_m(),
+nbDays_m(),
+vBooks_m(),
+vLibraries_m()
 {
 }
 
-PizzaProblem::~PizzaProblem(){
-	for (Pizza* pPizza_l : pizzas_m){
-		delete pPizza_l;
+BookProblem::~BookProblem(){
+	for (Book* pBook_l : vBooks_m){
+		delete pBook_l;
 	}
 }
 
 //Reader from input file
-istream& operator>>(istream& is_p, PizzaProblem& pizzaProblem_p){
-	is_p >> pizzaProblem_p.maxSlices_m >> pizzaProblem_p.nbPizzas_m;
+istream& operator>>(istream& is_p, BookProblem& bookProblem_p){
+	is_p >> bookProblem_p.nbBooks_m >> bookProblem_p.nbLibraries_m >> bookProblem_p.nbDays_m;
 
-	for (int i = 0; i < pizzaProblem_p.nbPizzas_m; i++){
-		int sliceSize_l;
-		is_p >> sliceSize_l;
-		Pizza* pNewPizza_l = new Pizza(i,sliceSize_l);
-		pizzaProblem_p.pizzas_m.push_back(pNewPizza_l);
+	for (int i = 0; i < bookProblem_p.nbBooks_m; i++){
+		int score_l;
+		is_p >> score_l;
+		bookProblem_p.vBooks_m.push_back(new Book(i,score_l));
+	}
+
+	for (int i = 0; i < bookProblem_p.nbLibraries_m; i++){
+		int nbBooksInLibrary_l;
+		int nbDaysToFinish_l;
+		int nbBooksShippedByDay_l;
+		is_p >> nbBooksInLibrary_l >> nbDaysToFinish_l >> nbBooksShippedByDay_l;
+		Library* pNewLibrary_l = new Library(i,nbBooksInLibrary_l,nbDaysToFinish_l,nbBooksShippedByDay_l);
+
+		for (int j = 0; j < nbBooksInLibrary_l; j++){
+			int bookIndex_l;
+			is_p >> bookIndex_l;
+			pNewLibrary_l->vBooksInLibrary_m.push_back(bookProblem_p.vBooks_m[bookIndex_l]);
+		}
+
+		bookProblem_p.vLibraries_m.push_back(pNewLibrary_l);
 	}
 
 	return is_p;
 }
 
 //Writer in output file
-ostream& operator<<(ostream& os_p, const PizzaProblem& pizzaProblem_p){
+ostream& operator<<(ostream& os_p, const BookProblem& bookProblem_p){
 
-	os_p << pizzaProblem_p.pizzasToOrder_m.size() << endl;
 
-	os_p << pizzaProblem_p.pizzasToOrder_m[0]->index_m;
+	os_p << bookProblem_p.nbBooks_m << " " << bookProblem_p.nbLibraries_m << " " << bookProblem_p.nbDays_m << endl;
 
-	for (unsigned int i = 1; i < pizzaProblem_p.pizzasToOrder_m.size(); i++){
-		os_p << " " << pizzaProblem_p.pizzasToOrder_m[i]->index_m;
+	for (int i = 0; i < bookProblem_p.nbBooks_m; i++){
+		os_p << bookProblem_p.vBooks_m[i]->score_m << " ";
 	}
+
+	os_p << endl;
+
+	for (int i = 0; i < bookProblem_p.nbLibraries_m; i++){
+		Library* pLibrary_l = bookProblem_p.vLibraries_m[i];
+		os_p << pLibrary_l->nbBooks_m << " " << pLibrary_l->nbDaysToFinish_m << " " << pLibrary_l->nbBooksShippedByDay_m << endl;
+		for (int j = 0; j < pLibrary_l->nbBooks_m; j++){
+			os_p << pLibrary_l->vBooksInLibrary_m[j]->index_m << " ";
+		}
+		os_p << endl;
+	}
+
+//	os_p << bookProblem_p.BooksToOrder_m.size() << endl;
+//
+//	os_p << bookProblem_p.BooksToOrder_m[0]->index_m;
+//
+//	for (unsigned int i = 1; i < bookProblem_p.BooksToOrder_m.size(); i++){
+//		os_p << " " << bookProblem_p.BooksToOrder_m[i]->index_m;
+//	}
 
 	return os_p;
 }
 
-int PizzaProblem::eval(){
-	int value_l = 0;
-	for (Pizza* pPizza_l : pizzasToOrder_m){
-		value_l+= pPizza_l->nbSlices_m;
-	}
-	cout << "eval = " << value_l << "\n";
-	return value_l;
+int BookProblem::eval(){
+//	int value_l = 0;
+//	for (Book* pBook_l : BooksToOrder_m){
+//		value_l+= pBook_l->nbSlices_m;
+//	}
+//	cout << "eval = " << value_l << "\n";
+//	return value_l;
+	return 0;
 }
 
-void opt(PizzaProblem pb_p, int index_p, int max_p){
+void opt(BookProblem pb_p, int index_p, int max_p){
 }
 
-void PizzaProblem::optimize(){
-	int sum_l = 0;
-	bool continue_l = true;
-	//Sort using a lambda
-	sort(pizzas_m.begin(),pizzas_m.end(),[](Pizza* p1_p, Pizza* p2_p){return p1_p->nbSlices_m > p2_p->nbSlices_m;});
-	//While loop
-	while(continue_l){
-		continue_l = false;
-		for (Pizza* pPizza_l : pizzas_m){
-			if (!pPizza_l->isAssigned_m && sum_l + pPizza_l->nbSlices_m < maxSlices_m){
-				pPizza_l->isAssigned_m = true;
-				pizzasToOrder_m.push_back(pPizza_l);
-				sum_l += pPizza_l->nbSlices_m;
-				continue_l = true;
-			}
-		}
-	}
+void BookProblem::optimize(){
+//	int sum_l = 0;
+//	bool continue_l = true;
+//	//Sort using a lambda
+//	sort(Books_m.begin(),Books_m.end(),[](Book* p1_p, Book* p2_p){return p1_p->nbSlices_m > p2_p->nbSlices_m;});
+//	//While loop
+//	while(continue_l){
+//		continue_l = false;
+//		for (Book* pBook_l : Books_m){
+//			if (!pBook_l->isAssigned_m && sum_l + pBook_l->nbSlices_m < maxSlices_m){
+//				pBook_l->isAssigned_m = true;
+//				BooksToOrder_m.push_back(pBook_l);
+//				sum_l += pBook_l->nbSlices_m;
+//				continue_l = true;
+//			}
+//		}
+//	}
 }
 
-void readInput(const string& fileName_p,PizzaProblem& pb_p){
+void readInput(const string& fileName_p,BookProblem& pb_p){
         //Open stream
         ifstream myFile_l;
         myFile_l.open(fileName_p.c_str());
@@ -131,7 +187,7 @@ void readInput(const string& fileName_p,PizzaProblem& pb_p){
         myFile_l.close();
 }
 
-void writeOutput(const string& inputFilename_p, const PizzaProblem& pb_p, int eval_p){
+void writeOutput(const string& inputFilename_p, const BookProblem& pb_p, int eval_p){
         //Open stream
         ofstream myFile_l;
         string inputFilename_l = inputFilename_p.substr(0,inputFilename_p.find("."));
@@ -146,18 +202,21 @@ void writeOutput(const string& inputFilename_p, const PizzaProblem& pb_p, int ev
 int main(int argc, char** argv){
 	//Problem creation
 	int total_l = 0;
+
 	for (int i = 1; i < argc; i++){
-		PizzaProblem pb_l;
+		BookProblem pb_l;
 		//Read file
 		readInput(string(argv[i]),pb_l);
 		//Optimize
-		pb_l.optimize();
-		//Eval
-		int eval_l = pb_l.eval();
-		total_l += eval_l;
-		//Write
+//		pb_l.optimize();
+//		//Eval
+//		int eval_l = pb_l.eval();
+//		total_l += eval_l;
+//		//Write
+		int eval_l = 0;
 		writeOutput(string(argv[i]),pb_l,eval_l);
 	}
+
 	cout << "Total = " << total_l << endl;
 	return 0;
 };
